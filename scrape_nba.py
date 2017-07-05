1eb593d63bd885673a7d62a7c537c32a1f7fca9b
Kevin <kw2ae@virginia.edu>
2017-07-04 20:14:09 -0400

    Added K-means and Mean Shift Classification

--- scrape_nba.py
+++ scrape_nba.py
@@ -5,11 +5,17 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator, IndexFormatter
 import re
 import sqlite3
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.cluster import MeanShift
+
 
 """gets the counting per game stats of all players in a current year, sort by
 any per game counting stat you choose"""
 
-def get_all_players_stats_year(year, sort_by):
+def get_all_players_stats_year(year, sort_by='PS/G'):
 	""" will return a dataframe that consists of all the stats of every player for a single year """
 
 	url = 'http://www.basketball-reference.com/leagues/NBA_'+str(year)+'_per_game.html'
@@ -314,7 +320,121 @@ def get_player_link(path_to_db, player_name, table_name, column_links='LINK', co
 	all_rows = c.fetchall()[0]	
 	return (all_rows)[0]
 
+#workaround to find dataframe by position
+def position_dataframe(df, year, position):
+	if 'g' in position.lower():
+		df = df.drop(df[df['Pos'] == 'SF'].index)
+		df = df.drop(df[df['Pos'] == 'PF'].index)
+		df = df.drop(df[df['Pos'] == 'SF-PF'].index)
+		df = df.drop(df[df['Pos'] == 'PF-SF'].index)
+		df = df.drop(df[df['Pos'] == 'PF-C'].index)
+		df = df.drop(df[df['Pos'] == 'C'].index)
+		df = df.drop(df[df['Pos'] == 'C-PF'].index)
+	if 'f' in position.lower():
+		df = df.drop(df[df['Pos'] == 'PG'].index)
+		df = df.drop(df[df['Pos'] == 'SG'].index)
+		df = df.drop(df[df['Pos'] == 'PG-SG'].index)
+		df = df.drop(df[df['Pos'] == 'SG-PG'].index)
+		df = df.drop(df[df['Pos'] == 'C'].index)
+	if 'c' in position.lower():
+		df = df.drop(df[df['Pos'] == 'PG'].index)
+		df = df.drop(df[df['Pos'] == 'SG'].index)
+		df = df.drop(df[df['Pos'] == 'SF'].index)
+		df = df.drop(df[df['Pos'] == 'PF'].index)
+		df = df.drop(df[df['Pos'] == 'PG-SG'].index)
+		df = df.drop(df[df['Pos'] == 'SG-PG'].index)
+		df = df.drop(df[df['Pos'] == 'PF-SS'].index)
+		df = df.drop(df[df['Pos'] == 'SF-PF'].index)
+	
+	return df
 
+#get a dataframe consisting of only 2 specific statistics
+def statistic_df(df, year, statistic_1, statistic_2):
+	df = df[['Player', statistic_1, statistic_2]]
+	df = df.drop(df[df[statistic_1].isnull()].index)
+	df = df.drop(df[df[statistic_2].isnull()].index)
+	return df
+
+#get dataframe of two specific stats for one position ...SLOW right now
+def position_statistic_df(year, statistic_1, statistic_2, position):
+	df = get_all_players_stats_year(year, 'PS/G')
+	df = position_dataframe(df, year, position)
+	df = statistic_df(df, year, statistic_1, statistic_2)
+	return df
+
+#convert a dataframe to a numpy array
+def df_to_array(df):
+	return df.values
+
+#find clusters based on distance away from centroids
+def KMeans_classify(array,clusters):
+	#separate the player name from the statistic
+	players = []
+	stats = []
+	for val in array:
+		players.append(val[0])
+		temp_stats = []
+		temp_stats.append(val[1])
+		temp_stats.append(val[2])
+		stats.append(temp_stats)
+	
+	#create the classifier
+	clf = KMeans(n_clusters=clusters)
+	clf.fit(stats)
+	
+	#get the centroids and labels
+	centroids = clf.cluster_centers_
+	labels = clf.labels_
+
+	player_labels = {}
+	for i in range(len(labels)):
+		player_labels[players[i]] = labels[i]
+
+	colors = ['g.', 'r.', 'c.', 'y.']
+	for i in range(len(stats)):
+		plt.plot(stats[i][0], stats[i][1], colors[labels[i]], markersize = 10)
+		# plt.annotate(players[i], xy=(stats[i][0], stats[i][1]))
+	plt.scatter(centroids[:, 0],centroids[:, 1], marker = "x", s=150, linewidths = 5, zorder = 10)
+	plt.show()
+
+	return player_labels
+
+def Mean_Shift_classify(array):
+	players = []
+	stats = []
+	for val in array:
+		players.append(val[0])
+		temp_stats = []
+		temp_stats.append(val[1])
+		temp_stats.append(val[2])
+		stats.append(temp_stats)
+	
+	#create classifier
+	ms = MeanShift()
+	ms.fit(stats)
+	labels = ms.labels_
+	cluster_centers = ms.cluster_centers_
+
+	player_labels = {}
+	for i in range(len(labels)):
+		player_labels[players[i]] = labels[i]
+
+	print(cluster_centers)
+	n_clusters_ = len(np.unique(labels))
+	print("Number of estimated clusters:", n_clusters_)
+
+	colors = 10*['r','g','b','c','k','y','m']
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+
+	for i in range(len(stats)):
+		ax.scatter(stats[i][0], stats[i][1], c=colors[labels[i]], marker='o')
+	ax.scatter(cluster_centers[:,0],cluster_centers[:,1],
+            marker="x",color='k', s=150, linewidths = 5, zorder=10)
+	plt.show()
+	
+	return player_labels
+# def numpy_statistic_array(year, statistic_1, statistic_2, position):
 if __name__=='__main__':
 	# get_player_stats_year(2017, 'PS/G')
 	# dict_of_players()
@@ -330,13 +450,25 @@ if __name__=='__main__':
 	# print get_accolades('jordami01')
 	# d = dict_of_players(2014,2015)
 	# create_link_name_db('/Users/Kevin/Desktop/test_nba.sqlite', d, 'PlayerLinks')
-	a =  get_player_link('/Users/Kevin/Desktop/test_nba.sqlite', 'pAUl GeoRGE', 'PlayerLinks')
+	# a =  get_player_link('/Users/Kevin/Desktop/test_nba.sqlite', 'pAUl GeoRGE', 'PlayerLinks')
 	# print a
-	data, player = get_individual_player_career_per_game_stats(a)
+	# data, player = get_individual_player_career_per_game_stats(a)
 	# print data
-	plot_dataframe(data, 'PTS', 'Paul George PTS', 'Season', 'PTS', 'Season', 'line')
+	# plot_dataframe(data, 'PTS', 'Paul George PTS', 'Season', 'PTS', 'Season', 'line')
+	# df = position_dataframe(2015, 'Guard')
+	# print statistic_array(2015, 'AST', 'TOV') 
+	style.use('ggplot')
+	# X = df_to_array(position_statistic_df(2015, 'AST', 'TOV', 'Guard'))
 
+	# Y = df_to_array(position_statistic_df(2015, 'FG%', 'PS/G', 'Guard'))
+	Z = df_to_array(position_statistic_df(2015, '3P%', '3P', 'Guard'))
+	# W = df_to_array(position_statistic_df(2015, 'ORB', 'DRB', 'Guard'))
 
+	# plt.scatter(X[:, 1],X[:, 2], s=15, c = 'b', alpha=0.7, linewidths = 5, zorder = 10)
+	# plt.show()
+	# threes = KMeans_classify(Z, 4)
+	threes = Mean_Shift_classify(Z)
+	print threes['Stephen Curry']
 
 
 
